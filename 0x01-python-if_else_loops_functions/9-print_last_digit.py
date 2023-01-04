af3a57e20ab63e015e9a3bdccb810a00ff582c64
umohammed4u <umohammed4u@gmail.com>
2023-01-04 13:38:17 -0800

    printlastdigit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+#!/usr/bin/python3
+# Author - Umar Mohammed
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
