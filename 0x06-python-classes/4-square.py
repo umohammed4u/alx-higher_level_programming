c491e80b92fadb9a9d28174336e800e38829c71a
umohammed4u <umohammed4u@gmail.com>
2023-01-25 15:02:24 -0800

    squared

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+Square = __import__('4-square').Square
 
-class Square:
-    """Represent a square."""
 
-    def __init__(self, size=0):
-        """Initialize a new square.
 
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
+my_square = Square(89)
 
-    @property
-    def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
 
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+
+my_square.size = 3
+
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+
+
+try:
+
+    my_square.size = "5 feet"
+
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+except Exception as e:
+
+    print(e)
