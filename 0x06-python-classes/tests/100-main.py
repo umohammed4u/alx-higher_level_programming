c95ded747c597873cf7c6b580f02c0b572de98c0
umohammed4u <umohammed4u@gmail.com>
2023-01-25 14:47:37 -0800

    mainpy

--- 0x06-python-classes/tests/100-main.py
+++ 0x06-python-classes/tests/100-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
+
 sll = SinglyLinkedList()
 sll.sorted_insert(2)
 sll.sorted_insert(5)
@@ -12,4 +13,5 @@ sll.sorted_insert(4)
 sll.sorted_insert(5)
 sll.sorted_insert(12)
 sll.sorted_insert(3)
+
 print(sll)
