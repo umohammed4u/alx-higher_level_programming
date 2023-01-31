bcb8a2ff8eb7078b502621a24fc30512b432dbee
umohammed4u <umohammed4u@gmail.com>
2023-01-31 13:37:29 -0800

    1rectangl

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """Represent a rectangle."""
+    
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
@@ -12,10 +13,12 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+    
     @property
     def width(self):
         """Get/set the width of the rectangle."""
         return self.__width
+    
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -23,10 +26,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+    
     @property
     def height(self):
         """Get/set the height of the rectangle."""
         return self.__height
+    
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
