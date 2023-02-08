1b873d79349933d2fb44d835b9874e3170656b46
umohammed4u <umohammed4u@gmail.com>
2023-02-08 12:29:50 -0800

    11student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -13,6 +13,7 @@ class Student:
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """        
+        
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
