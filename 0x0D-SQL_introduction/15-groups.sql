1eedf70e666541fc26898b42b6ca5ee313ae0729
umohammed4u <umohammed4u@gmail.com>
2023-03-15 21:58:12 +0000

    15groups

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
--- Computes the average score of all records in the table second_table in my MySQL server.
-SELECT AVG(`score`) AS `average`
-FROM `second_table`;
+-- Lists the number of records with the same score in the table second_table in my MySQL server.
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
