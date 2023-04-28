b08881c1d2b5f086f02c500e20eadb8fbd68661a
umohammed4u <umohammed4u@gmail.com>
2023-04-28 15:10:11 -0700

    3errorcde

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""This script takes in a URL, sends a request to the URL and
-displays the body of the response (decoded in utf-8)
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
 """
 
-from urllib import request
-from urllib.error import HTTPError
-import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    req = request.Request(url)
-    try:
-        with request.urlopen(req) as response:
-            print(response.read().decode('utf8'))
-    except HTTPError as e:
-        print("Error code: {}".format(e.code))
+    import sys
+    from urllib import request, error
 
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
